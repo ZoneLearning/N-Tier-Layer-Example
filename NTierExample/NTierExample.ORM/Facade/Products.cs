2e8c1af7e0cc4b9aca5df1736a5fe3471c000f11
ZoneLearning <myselflearningzone@gmail.com>
2016-07-08 23:50:13 +0300

    App config'den server, database, user,password bileşenleri eklendi. Helper sınfına dikkat

---
+++ NTierExample/NTierExample.ORM/Facade/Products.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NTierExample.ORM.Facade
+{
+    //Ürünler ile ilgili insert, update, delete, select işlemlerini (metodlarını) barındıran sınıf
+   public class Products
+    {
+        public static DataTable getList()
+        {
+            return null;
+        }
+    }
+}
---
+++ NTierExample/NTierExample.ORM/Helper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+
+namespace NTierExample.ORM
+{
+    //sadece bu projeden ulaşılacağı için public olmasına gerek yok.
+    class Helper  // internal
+    {
+        private static SqlConnection connection = new SqlConnection(ConfigurationManager.ConnectionStrings["NorthWind"].ConnectionString);
+
+        public static SqlConnection Connection
+        {
+            get { return connection; }
+            set { connection = value; }
+        }
+
+    }
+}
